4bf5559c83d9097e8ecda123ce3c44e1500fcb09
stevenmwang <swang7@stanford.edu>
2016-02-11 19:01:56 -0800

    Fixed SQL mining

--- GotYou/GotYou/EventTableViewCell.swift
+++ GotYou/GotYou/EventTableViewCell.swift
@@ -12,6 +12,7 @@ class EventTableViewCell: UITableViewCell {
 
     @IBOutlet weak var location: UILabel!
     @IBOutlet weak var eventDesc: UILabel!
+
     @IBOutlet weak var duration: UILabel!
     @IBOutlet weak var cost: UILabel!
     @IBOutlet weak var numOrders: UILabel!
