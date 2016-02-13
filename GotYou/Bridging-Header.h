6825ffcc5be0032b82d89a8ff051346e0adb3ca3
stevenmwang <swang7@stanford.edu>
2016-02-13 01:09:13 -0800

    Added bridging header

---
+++ GotYou/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  GotYou
+//
+//  Created by Steven Wang on 2/13/16.
+//  Copyright © 2016 Stanford. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import "FBSDKLoginKit/FBSDKLoginKit.h"
+#endif /* Bridging_Header_h */
