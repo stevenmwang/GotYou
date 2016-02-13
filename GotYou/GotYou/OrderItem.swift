4e42f16a2275de6448c8d0ef5ba914035804af87
bcui19 <bcui19@stanford.edu>
2016-02-13 04:54:27 -0800

    added order class

---
+++ GotYou/GotYou/OrderItem.swift
@@ -0,0 +1,13 @@
+//
+//  OrderItem.swift
+//  GotYou
+//
+//  Created by Steven Wang on 2/13/16.
+//  Copyright © 2016 Stanford. All rights reserved.
+//
+
+import Cocoa
+
+class OrderItem: NSObject {
+
+}
