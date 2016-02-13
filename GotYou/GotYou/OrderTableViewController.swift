ed863869a15ced63a1349f3580a96b98ed2a7da6
bcui19 <bcui19@stanford.edu>
2016-02-13 05:12:21 -0800

    Creating parsing function for events

---
+++ GotYou/GotYou/OrderTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  OrderTableViewController.swift
+//  GotYou
+//
+//  Created by Steven Wang on 2/13/16.
+//  Copyright © 2016 Stanford. All rights reserved.
+//
+
+import Cocoa
+
+class OrderTableViewController: NSObject {
+
+}
