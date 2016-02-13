0555ec548ac3e912da7b68636f25b0e7ae5a8714
bcui19 <bcui19@stanford.edu>
2016-02-13 04:58:52 -0800

    Order Class Added

--- GotYou/GotYou/OrderItem.swift
+++ GotYou/GotYou/OrderItem.swift
@@ -1,13 +1,26 @@
 //
-//  OrderItem.swift
+//  OrderClass.swift
 //  GotYou
 //
 //  Created by Steven Wang on 2/13/16.
 //  Copyright © 2016 Stanford. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
-class OrderItem: NSObject {
-
+class OrderClass: NSObject {
+    var orderID: String
+    var userID: Int
+    var eventID: Int
+    var quantity: Int
+    var cost: Double
+    
+    init (orderID: String, userID: Int, eventID: Int,
+        quantity: Int, cost: Double) {
+            self.orderID = orderID
+            self.userID = userID
+            self.eventID = eventID
+            self.quantity = quantity
+            self.cost = cost
+    }
 }
